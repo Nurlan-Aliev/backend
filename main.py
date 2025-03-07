4424a83d5f4e06c33cafacad79b90b413608c848
Nurlan-Aliev <nualiev612@gmail.com>
2025-03-07 16:39:49 +0400

    create database, and connect

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.books.views import router as book_router
+from app.database import db_helper
+from app.books.models import Base
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost",
@@ -16,7 +29,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get("/books")
-def books():
-    return []
+app.include_router(book_router, prefix="/books")
