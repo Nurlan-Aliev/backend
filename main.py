e34df265f682ec779a1137ce9345668acd1ce7cd
Nurlan-Aliev <nualiev612@gmail.com>
2025-03-23 04:27:19 +0400

    docker compose file

--- main.py
+++ main.py
@@ -29,5 +29,4 @@ app.include_router(book_router, prefix="/books")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
- 
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
